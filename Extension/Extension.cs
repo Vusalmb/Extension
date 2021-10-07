44e5979162f10ac04104c9d1f5651db70177cbe2
Vusalmb <vusalmb@code.edu.az>
2021-10-07 11:34:46 +0400

    Добавьте файлы проекта.

---
+++ Extension/Extension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension
+{
+    static class Extension
+    {
+        public static bool CustomContain(this string str, string word)
+        {
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                for (int j = 0; j < word.Length; j++)
+                {
+                    if (str[i] == word[j])
+                    {
+                        result.Append(str[i]);
+                    }
+
+                }
+            }
+            if (result.Length != word.Length)
+            {
+                //burada ilişdim
+            }
+            return false;
+        }
+    }
+}
---
+++ Extension/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello world how are you";
+            Console.WriteLine(str.CustomContain("ow"));
+        }
+    }
+}
